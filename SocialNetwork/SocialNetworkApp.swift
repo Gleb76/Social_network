5566199261d0f402b06351c6aae54ff738b71d55
Gleb Klyga <klyga90@bk.ru>
2024-10-26 22:57:09 +0300

    Initial Commit

---
+++ SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by Глеб Клыга on 26.10.24.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
